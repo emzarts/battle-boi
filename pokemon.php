9a5824622edec0e0b7fc93618a2d0354b27754c3
emzarts <17zimmermane@gmail.com>
2020-04-14 14:25:03 -0400

    display pokemon name and photo

--- pokemon.php
+++ pokemon.php
@@ -8,13 +8,28 @@
     <?php 
     $pokemon = $_GET['pokemon-name'];
     $json = file_get_contents("https://pokeapi.co/api/v2/pokemon/$pokemon/");
-    //echo $json;
     $pokemon_details = json_decode($json);
-    echo "types: ";
+
+    // display message if pokemon is not found
+    if(!$json) {
+      echo "$pokemon not found.";
+      return;
+    }
+    echo "<h1>$pokemon</h1>";
+    $sprite = $pokemon_details->{"sprites"}->{"front_default"};
+    echo "<img src=\"$sprite\" alt=\"image of $pokemon\"/>";
+    echo "<br>";
+    echo "<strong>Types: </strong>";
     foreach($pokemon_details->{"types"} as $type) {
       echo $type->{"type"}->{"name"} . " ";
     }
-    //.$pokemon_details->{"types"}[1]->{"type"}->{"name"};
+    echo "<br> <strong>Stats:</strong> <br>";
+    //var_dump($pokemon_details->{"stats"});
+    foreach($pokemon_details->{"stats"} as $stat) {
+      echo $stat->{"stat"}->{"name"} . ": ";
+      echo $stat->base_stat;
+      echo "<br>";
+    }
     ?>
   </body>
 </html>
